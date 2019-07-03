d0968d32aa23eece1d869e340000035f3a7b3da0
Cyber Walrus <stonewalrusfan@gmail.com>
2019-07-03 03:59:52 +0300

    add with-task-state

--- client/components/page-time/page-time.tsx
+++ client/components/page-time/page-time.tsx
@@ -1,5 +1,9 @@
+import withTaskState, {
+  PublicFunction,
+  State,
+} from "@client/hocs/with-task-state/with-task-state";
 import * as React from "react";
-import { FunctionComponent, ReactElement } from "react";
+import { ComponentClass, FunctionComponent, ReactElement } from "react";
 interface Time {
   minute: number;
   text: string;
@@ -15,12 +19,19 @@ const getTimes = (
     const timeString = `${hour.toString().length < 2 ? `0${hour}` : hour}:${
       minute.toString().length < 2 ? `0${minute}` : minute
     }`;
-    times.push({ minute: i, text: timeString });
+    times.push({minute: i, text: timeString});
   }
   return times;
 };
 
-const PageTime: FunctionComponent = (): ReactElement => {
+type PropsHoc = State & PublicFunction;
+type Props = PropsHoc;
+
+const PageTime: FunctionComponent<PropsHoc> = ({
+  tasks,
+  onAddTask,
+  onChangeTask,
+}: Props): ReactElement => {
   const times = getTimes(60);
   return (
     <main className={`page-content`}>
@@ -30,15 +41,24 @@ const PageTime: FunctionComponent = (): ReactElement => {
       <section>
         <div className={`time-table`}>
           {times &&
-            times.map(({ minute, text }: Time, index: number) => (
+            times.map(({minute, text}: Time, index: number) => (
               <React.Fragment key={index}>
                 <div className={`time-table__time`}>{text}</div>
               </React.Fragment>
             ))}
+          {tasks &&
+            tasks.map((task: string, index: number) => (
+              <React.Fragment key={index}>
+                <div className={`time-table__task`}>{task}</div>
+              </React.Fragment>
+            ))}
         </div>
+        <button onClick={onAddTask}>+</button>
       </section>
     </main>
   );
 };
 
-export default PageTime;
+export { PageTime };
+
+export default withTaskState(PageTime) as ComponentClass;
--- client/hocs/with-task-state/with-task-state.tsx
+++ client/hocs/with-task-state/with-task-state.tsx
@@ -1,10 +1,13 @@
 import * as React from "react";
 import { ComponentClass, PureComponent, ReactElement, RefObject } from "react";
 
-interface State {
+export interface State {
   tasks: string[];
 }
-
+export interface PublicFunction {
+  onAddTask: () => void;
+  onChangeTask: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
 const withTaskState = (Component: any): ComponentClass => {
   type P = ReturnType<typeof Component>;
 
@@ -12,16 +15,16 @@ const withTaskState = (Component: any): ComponentClass => {
     public constructor(props: P) {
       super(props);
       this.state = {
-        tasks: [],
+        tasks: [`test1`],
       };
       this.handlAddTask = this.handlAddTask.bind(this);
       this.handlChangeTask = this.handlChangeTask.bind(this);
     }
     public handlAddTask(): void {
-      const tasks = this.state.tasks;
-      tasks.push(``);
+      const tasksNew = this.state.tasks.slice();
+      tasksNew.push(`test`);
       this.setState({
-        tasks,
+        tasks: tasksNew,
       });
     }
     public handlChangeTask(event: React.ChangeEvent<HTMLInputElement>): void {
@@ -32,7 +35,7 @@ const withTaskState = (Component: any): ComponentClass => {
       return (
         <Component
           {...this.props}
-          tasks={this.props.tasks}
+          tasks={this.state.tasks}
           onAddTask={this.handlAddTask}
           onChangeTask={this.handlChangeTask}
         />
